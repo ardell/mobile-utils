1280ec053f930beaf1f319d537dec7b54137e08b
Alan Pinstein <apinstein@mac.com>
2011-07-29 13:24:57 -0400

    convert to a simpler 1-page php script.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Orientation::Application.config.secret_token = '78ea733f9f5d2dd3bbd47fa664b7a9794ca09862813c5dbeedbb56e511bad4d32c6dc4003db784872f76a6154cca8e1e55d15c533f9d43454e49bd47fc4a54ef'
