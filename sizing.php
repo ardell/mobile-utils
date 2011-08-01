01ff4708d7c28fce40c7ce9e8f36545681d474c5
Alan Pinstein <apinstein@mac.com>
2011-08-01 10:32:49 -0400

    add another scroll option

--- sizing.php
+++ sizing.php
@@ -206,7 +206,7 @@ updateInfo('head', true);
 </ul>
 
 <div id="contents">
-  <p><a href="#" onclick="window.scrollTo(0,0); return false;">scrollTo(0,0)</a></p>
+  <p><a href="#" onclick="window.scrollTo(0,0); return false;">scrollTo(0,0)</a> <a href="#" onclick="window.scrollTo(0,1); return false;">scrollTo(0,1)</a></p>
   <p><a href="#" onclick="updateInfo('manual click'); return false;">Update</a></p>
 
   <div id="info"></div>
