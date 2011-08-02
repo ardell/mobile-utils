2a1f92c21d9f8524a7ef9caafe7d0f0b9705815a
Jason Ardell <ardell@gmail.com>
2011-08-02 14:32:47 -0400

    WIP: Debugging when window.scrollTop gets fired.

--- javascripts/jquery.fullScreenAppDimensionsChanged.js
+++ javascripts/jquery.fullScreenAppDimensionsChanged.js
@@ -1,59 +1,75 @@
 window.doScroll = function() {
-  window.scrollTo(0, 3);
-  console.log("begin doScroll from: " + jQuery('body').scrollTop() + " at: " + (+new Date()));
+  // Things that don't work:
+  // - document.body.scrollTop
+  // - window.pageYOffset
+  // - document.body.parentElement.scrollTop
+  // - jQuery(window).scrollTop()
+  // - jQuery('html').scrollTop()
+  // - jQuery(document).scrollTop()
+  // - jQuery('body').scrollTop()
+  // For both:
+  // - jQuery(window).scrollTop(3);
+  // - window.scrollTo(0, 3);
+  // window.scrollTo(0, 3);
+  // console.log("Really big div: " + jQuery('.reallyBigDiv').width() + "x" + jQuery('.reallyBigDiv').height());
+  console.log("begin doScroll from: " + window.pageYOffset + " at: " + (+new Date()));
   if (MobileUtilities.isAndroid())
   {
     window.scrollTo(0, 1);
   } else {
     window.scrollTo(0, 0);
   }
-  console.log("end doScroll at: " + jQuery('body').scrollTop() + " at: " + (+new Date()));
+  console.log("end doScroll at: " + window.pageYOffset + " at: " + (+new Date()));
 };
 
 var $window = jQuery(window);
 jQuery.event.special.deviceIndependentOrientationChange = {
   _currentOrientation: null,
   add: function(handleObj) {
-    var elem     = this, $elem = jQuery(elem);
-    var _handler = function(event) {
-      var customEvent = jQuery.Event("deviceIndependentOrientationChange");
-      $window.trigger(customEvent);
-    };
+    jQuery(document).ready(function() {
+      var elem     = this, $elem = jQuery(elem);
+      var _handler = function(event) {
+        var customEvent = jQuery.Event("deviceIndependentOrientationChange");
+        $window.trigger(customEvent);
+      };
 
-    // Initialize _currentOrientation
-    _currentOrientation = MobileUtilities.orientation();
+      // Initialize _currentOrientation
+      _currentOrientation = MobileUtilities.orientation();
 
-    if (MobileUtilities.isIos())
-    {
-      // iOS throws orientationchange events correctly
-      // (i.e. after window dimensions have changed)
-      $elem.bind(
-        'orientationchange',
-        _handler
-      );
-    } else if (!Modernizr.touch) {
-      $elem.bind(
-        'resize',
-        _handler
-      );
-    } else {
-      // Android throws orientationchange events BEFORE
-      // window.innerHeight has changed, so we have to
-      // watch resize events and track whether the
-      // orientation has changed.
-      $elem.bind(
-        'resize',
-        function(event) {
-          // Don't fire duplicates
-          var newOrientation = MobileUtilities.orientation();
-          if (newOrientation == _currentOrientation) return;
-          _currentOrientation = newOrientation;
+      if (MobileUtilities.isIos())
+      {
+        // iOS throws orientationchange events correctly
+        // (i.e. after window dimensions have changed)
+        $elem.bind(
+          'orientationchange',
+          _handler
+        );
+      } else if (!Modernizr.touch) {
+        $elem.bind(
+          'resize',
+          _handler
+        );
+      } else {
+        // Android throws orientationchange events BEFORE
+        // window.innerHeight has changed, so we have to
+        // watch resize events and track whether the
+        // orientation has changed.
+        $elem.bind(
+          'resize',
+          function(event) {
+            // Don't fire duplicates
+            var newOrientation = MobileUtilities.orientation();
+            if (newOrientation == _currentOrientation) return;
+            _currentOrientation = newOrientation;
 
-          // Run the handler
-          _handler(event);
-        }
-      );
-    }
+            // Run the handler
+            _handler(event);
+          }
+        );
+      }
+
+      _handler();
+    });
   },
 
   teardown: function(namespaces) {
@@ -79,9 +95,6 @@ jQuery.event.special.fullScreenAppDimensionsChanged = {
       'deviceIndependentOrientationChange',
       _handler
     );
-
-    // Fire once the first time
-    _handler();
   },
 
   teardown: function(namespaces) {
